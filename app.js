0fc47f8332d8ef3244ecbd7a71964557ec7c93f6
unknown <roger_lu_dude@hotmail.com>
2019-01-21 04:43:06 -0500

    Adding products to cart

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const Joi = require('joi');
 const app = express();
+const uuidv1 = require('uuid/v1');
 
 app.use(express.json());
 
-const products = [
+let carts = [];
+let products = [
     {title: "apple", price: 1, inventory_count: 3},
     {title: "orange", price: 2, inventory_count: 5},
-    {title: "banana", price: 1.5, inventory_count: 1},
-]
+    {title: "banana", price: 1.5, inventory_count: 2},
+];
 
 app.get('/', (req, res) => {
     res.send("Welcome to the fruit store");
@@ -35,21 +37,66 @@ app.get('/products/:product', (req, res) => {
     res.send(product);
 });
 
-// PURCHASE PRODUCTS
-app.put('/products/:product/buy', (req, res) => {
-    const product = products.find(p => p.title === req.params.product);
+// PURCHASE PRODUCT
+// request body: cart id
+app.post('/products/:product/addToCart', (req, res) => {
+    // find existing cart or create a new one
+    let cart = carts.find(c => c.id === req.body.id);
+    if (!cart) {
+        cart = setUpCart();
+    }
+    // find product or return error response
+    const productName = req.params.product;
+    const product = products.find(p => p.title === productName);
     if (!product) {
         return res.status(404).send("404: Product does not exist");
     }
     if (product.inventory_count === 0){
-        return res.status(400).send(`There is not more of this product: ${product.name}`);
+        return res.status(400).send(`There is not more of this product: ${product.title}`);
     }
-    if (product.inventory_count < req.params.count){
-        return res.status(400).send(`There is only ${inventory_count} of this product: ${product.title}`);
+    // find product in cart and increment
+    // or add product with 1 count
+    if (cart.items.find(item => item.title === productName)) {
+        const item = cart.items.find(item => item.title === productName);
+        if (item.count >= product.inventory_count) {
+            return res.status(400).send("400: You cannot add of more this product.");
+        }
+        item.count++;
+        res.send(cart);
     }
-    product.inventory_count--
-    res.send(product);
+    else {
+        const item = addToCart(product);
+        cart.items.push(item);
+        carts.push(cart);
+        res.send(cart);
+    }
 });
+// CART INFO
+// request body: cart id
+app.post('/cart', (req, res) => {
+    const cart = carts.find(c => c.id === req.body.id);
+    if (!cart) {
+        return res.status(404).send("404: Cart does not exist");
+    }
+    res.send(cart.items);
+})
+
+// CART
+function setUpCart() {
+    const cart = {};
+    cart.id = uuidv1();
+    cart.items = [];
+    return cart;
+};
+
+function addToCart(product) {
+    let item = {};
+    item.title = product.title;
+    item.price = product.price;
+    item.count = 1;
+    return item;
+}
+
 
 // function validateCount(product) {
 //     const schema = {
